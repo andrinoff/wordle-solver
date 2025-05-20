6c5c3da927ae53bdb06174e7b4f5fdc7c5e01256
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-05-20 16:51:38 +0400

    new: now searches for the letter position

--- brute_force/main.py
+++ brute_force/main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Brute_Force:
     def __init__(self):
-        self.words = np.loadtxt("../words.txt", dtype=str)
+        self.words = np.loadtxt("words.txt", dtype=str)
         self.letter1 = ""
         self.letter2 = ""
         self.letter3 = ""
@@ -10,7 +10,7 @@ class Brute_Force:
         self.letter5 = ""
         self.yellow = []
         self.green =  []
-        self.first_guess = ""
+        self.last_guess = ""
 
     def start(self):
         # Ask what word the user used and the letter marks
@@ -28,13 +28,31 @@ class Brute_Force:
             else :
                 break
         self.deleteYellows()
+        self.getPositionOfTheGreen("block")
 
-    def deleteYellows(self,):
+    def deleteYellows(self):
         for word in self.words:
             for letter in self.yellow:
                 if letter in word:
                     word_index = np.where(self.words == word)
-                    print(self.words[word_index])
+                    # TODO: add delete here
+
+
+# Caution: will ONLY work the the said letter repeats only once for now
+    def getPositionOfTheGreen(self, guess):
+        for letter in self.green:
+            for i in range(0,4):
+                if list(guess)[i] == letter:
+                    position_green = i
+                    print (i+1) # Prints the number the letter is found in
+                    for word in self.words:
+                        # print(list(word)[position_green] == self.green)
+                        if list(word)[position_green] == letter:
+                            word_index = np.where(self.words == word)
+                            # TODO: add delete here
+                            print (self.words[word_index])
+
+
 
     def test(self, word):
         print(self.words[word])
