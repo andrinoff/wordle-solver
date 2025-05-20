e89d6b598d500c85884f0cfbe0d47ac24410f784
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-05-20 21:32:40 +0400

    First edition: brute force

--- brute_force/main.py
+++ brute_force/main.py
@@ -13,44 +13,60 @@ class Brute_Force:
         self.last_guess = ""
 
     def start(self):
-        # Ask what word the user used and the letter marks
-        self.first_guess = input("What word did you use? ")
-        for i in range(0,5) :
-            yellow_letter = input("What is the yellow letter? n for none ")
-            if yellow_letter != "n":
-                self.yellow.append(yellow_letter)
-            else :
+        for i in range(0,5):
+            # Ask what word the user used and the letter marks
+            self.last_guess = input("What word did you use? ")
+            for i in range(0,4) :
+                yellow_letter = input("What is the yellow letter? 0 for none ")
+                if yellow_letter != "0":
+                    self.yellow.append(yellow_letter)
+                else :
+                    break
+            for i in range(0,4) :
+                green_letter = input("What is the green letter? 0 for none ")
+                if green_letter != "0":
+                    self.green.append(green_letter)
+                else :
+                    break
+            self.deleteYellows()
+            self.deleteGreens(self.last_guess)
+            print(self.words)
+            if len(self.words) <= 2:
                 break
-        for i in range(0,5) :
-            green_letter = input("What is the green letter? n for none ")
-            if green_letter != "n":
-                self.green.append(green_letter)
-            else :
-                break
-        self.deleteYellows()
-        self.getPositionOfTheGreen("block")
 
     def deleteYellows(self):
-        for word in self.words:
-            for letter in self.yellow:
-                if letter in word:
-                    word_index = np.where(self.words == word)
+        for letter in self.yellow:
+            for word in self.words:
+                # If none found -- delete
+                word_index = np.where(self.words == word)
+                if letter not in word:
+
                     # TODO: add delete here
+                    self.words = np.delete(self.words, word_index)
+                # Second round, deleting where the letter would be green
+                else:
+                    index = self.getPositionOfTheLetter(self.last_guess, letter)
+                    if list(word)[index] == letter:
+                        self.words = np.delete(self.words, word_index)
+        self.yellow = [l for l in self.yellow if l != letter]
+
+    def getPositionOfTheLetter(self, guess, letter):
+        return list(guess).index(letter)
+
+
 
 
 # Caution: will ONLY work the the said letter repeats only once for now
-    def getPositionOfTheGreen(self, guess):
+    def deleteGreens(self, guess):
         for letter in self.green:
-            for i in range(0,4):
-                if list(guess)[i] == letter:
-                    position_green = i
-                    print (i+1) # Prints the number the letter is found in
-                    for word in self.words:
-                        # print(list(word)[position_green] == self.green)
-                        if list(word)[position_green] == letter:
-                            word_index = np.where(self.words == word)
-                            # TODO: add delete here
-                            print (self.words[word_index])
+            position_green = self.getPositionOfTheLetter(guess, letter)
+            for word in self.words:
+                if list(word)[position_green] != letter:
+                    word_index = np.where(self.words == word)
+                    self.words = np.delete(self.words, word_index)
+        self.green = [l for l in self.green if l != letter]
+
+
 
 
 
